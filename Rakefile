904f7fc74bbefe96ae631bb8afe5d6b0ae67be1c
worlanyodamalie <worlalidamalie@gmail.com>
2016-11-17 16:36:55 +0000

    Migrate from Rails 4 to Rails 5
    Migrate to rails 5

--- Rakefile
+++ Rakefile
@@ -1,10 +1,6 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
-<<<<<<< HEAD
 require_relative 'config/application'
-=======
-require File.expand_path('../config/application', __FILE__)
->>>>>>> 92b8ccc04b504aaa0d2b14e313a2fd4f8d077aed
 
 Rails.application.load_tasks
--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -5,18 +5,10 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-<<<<<<< HEAD
   wrap_parameters format: [:json]
-=======
-  wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
->>>>>>> 92b8ccc04b504aaa0d2b14e313a2fd4f8d077aed
 end
 
 # To enable root element in JSON for ActiveRecord objects.
 # ActiveSupport.on_load(:active_record) do
-<<<<<<< HEAD
 #   self.include_root_in_json = true
-=======
-#  self.include_root_in_json = true
->>>>>>> 92b8ccc04b504aaa0d2b14e313a2fd4f8d077aed
 # end
