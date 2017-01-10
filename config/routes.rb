61fc06923ee589f69fdf756f5978015b4a27fdbd
Worlanyo Damalie <worlalidamalie@gmail.com>
2017-01-10 20:42:05 +0000

    Build static pages with links for home,help and about / Test the links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  resources :complaints
- root "complaints#index"
+ #root "complaints#index"
  # complaints "complaints#create"
  # new_complaints "complaints#new"
  # complaints "complaints#show"
